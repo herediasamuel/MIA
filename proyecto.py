109a458f8d23eae475e5c8982fc675840bbad302
Misael Zavala <65864332+BiackSpawn@users.noreply.github.com>
2022-07-21 00:04:17 -0400

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -19,10 +19,13 @@ df_ventas=pd.read_csv('datos_uc.csv',delimiter=';')
 
 
 venta_mensual_ori=pd.pivot_table(df_ventas,values=['Venta_Neta','Ofs'],index=['Fecha','Origen_Zona'],aggfunc=np.sum).reset_index()
+
+
 st.table(venta_mensual_ori)
 
+alt.selection_multi(fields=['Origen_Zona'], bind='legend')
+
 st.altair_chart(
-    alt.selection_multi(fields=['Origen_Zona'], bind='legend')
     alt.Chart(venta_mensual_ori).mark_area().encode(
         alt.X('yearmonth(Fecha Nominal):N', title='Fecha'),
         alt.Y('sum(Ofs):Q'),

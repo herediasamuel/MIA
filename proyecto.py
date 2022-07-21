10f5d79ffff1621ba714a24c878bab253f01af78
Misael Zavala <65864332+BiackSpawn@users.noreply.github.com>
2022-07-20 23:39:18 -0400

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -18,7 +18,7 @@ st.title('Proyecto Visualizacion')
 df_ventas=pd.read_csv('datos_uc.csv',delimiter=';')
 
 venta_mensual=pd.pivot_table(df_ventas,values=['Ofs'],index=['Fecha','Nombre_Categoria'],aggfunc=np.sum)
-
+st.table(venta_mensual)
 st.altair_chart(
     alt.Chart(venta_mensual).mark_bar().encode(
         alt.X('yearmonth(Fecha):N', title='Fecha'),

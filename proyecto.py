7c72dc233e7f5b3ebaeb6a4628e57256d60a4760
Misael Zavala <65864332+BiackSpawn@users.noreply.github.com>
2022-07-20 23:58:53 -0400

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -23,7 +23,7 @@ st.table(venta_mensual)
 
 st.altair_chart(
     alt.Chart(venta_mensual).mark_bar().encode(
-        alt.X('Ano:n', title='Fecha'),
+        alt.X('Ano:O', title='Fecha'),
         alt.Y('Venta_Neta:Q', title='Venta Neta Mensual'), 
     ).properties(
         width=1000,
